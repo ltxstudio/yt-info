cc89f192ec975e4efb83c394daf2d471ac50fe11
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-30 22:39:11 +0600

    Create Spinner.js

---
+++ components/Spinner.js
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-spin rounded-full h-24 w-24 border-t-4 border-blue-500"></div>
+    </div>
+  );
+};
